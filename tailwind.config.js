78e9ba414cef5878ace2b3588a30d76f73a36e95
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2025-12-31 04:50:25 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
     './assets/**/*.css',           // all CSS files with TW classes
     './_includes/**/*.{html,md}',   // if using Jekyll includes
     './_layouts/**/*.{html,md}',    // if using Jekyll layouts
-    './_pages/**/*.{html,md}',    // if using Jekyll layouts
+    './pages/**/*.{html,md}',    // if using Jekyll layouts
     './*.html',                // any top-level HTML
   ],
   theme: {
