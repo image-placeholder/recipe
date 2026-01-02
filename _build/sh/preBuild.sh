426247b04d8d1eb62eafb786e78aacde6603158d
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-01-02 05:56:08 -0500

    Add preBuild.sh for installing dependencies and compiling CSS

---
+++ _build/sh/preBuild.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo "Running post build script...."
+
+echo "▶ Installing Font Awesome"
+
+npm install --save @fortawesome/fontawesome-free
+npm install --save copy-webpack-plugin
+
+echo "✔ Font Awesome installed"
+
+echo "▶ Installing Tailwind CSS"
+npm install -g purgecss
+npm install tailwindcss@3.4.17 postcss autoprefixer
+echo "✔ Tailwind installed"
+echo "▶ Compiling Tailwind CSS"
+npx tailwindcss -i ./assets/css/_tailwind.css -o ./assets/css/tailwind.min.css --minify
+
+echo "✔ Tailwind Compiled To /assets/css/tailwind.min.css"
