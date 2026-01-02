6a097dfb790089cbf27cef789c9fe7ad3755955b
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-01-02 09:52:16 -0500

    Update preBuild.sh

--- _build/sh/preBuild.sh
+++ _build/sh/preBuild.sh
@@ -21,3 +21,4 @@ npx tailwindcss -i ./assets/css/_tailwind.css -o ./assets/css/tailwind.min.css -
 echo "✔ Tailwind Compiled To /assets/css/tailwind.min.css"
 
 npm install puppeteer
+gem install grover
