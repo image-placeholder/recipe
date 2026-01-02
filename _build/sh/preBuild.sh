6e0abf8a472351bac0915b9b334130813012c1e6
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-01-02 10:36:00 -0500

    Update preBuild.sh

--- _build/sh/preBuild.sh
+++ _build/sh/preBuild.sh
@@ -21,6 +21,7 @@ npx tailwindcss -i ./assets/css/_tailwind.css -o ./assets/css/tailwind.min.css -
 echo "✔ Tailwind Compiled To /assets/css/tailwind.min.css"
 
 # These are the Linux libraries Chrome needs to run in a headless environment
+sudo apt update
 sudo apt-get update
 sudo apt-get install -y libgbm-dev libnss3 libatk-bridge2.0-0 libgtk-3-0 libasound2
 npm install -g puppeteer
