30873ef8b45895e5468341e53fbf63696d2c7ec0
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2026-01-07 03:19:44 -0500

    Update postBuild.sh

--- _build/sh/postBuild.sh
+++ _build/sh/postBuild.sh
@@ -7,3 +7,12 @@ set -e
 # -----------------------------
 chmod +x _build/sh/commit_og_images.sh
 ./_build/sh/commit_og_images.sh
+
+
+
+# 2. Run Pagefind to index the built site
+# --site _site points to Jekyll's default output folder
+echo "Running Pagefind indexing..."
+npx -y pagefind --site _site
+
+echo "Build and Indexing complete!"
